9826f8cca5ab0fe0b6203798032a3da08328f53c
Flo <martiniflorent@gmail.com>
2021-06-04 22:55:28 +0200

    ajout accordeon home + projet + image responsive

--- js/accordion.js
+++ js/accordion.js
@@ -1,10 +1,43 @@
-var acc = document.getElementsByClassName("accordion");
+// ACCORDION CONTRIBUTION
+var acc = document.getElementsByClassName("accordion--contribution");
 
 for (var i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    var isActive = document.querySelector(".accordion").classList.contains("active");
+    var isActive = document.querySelector(".accordion--contribution").classList.contains("active");
+    if (isActive) {
+      panel.style.maxHeight = "480px";
+    } else {
+      panel.style.maxHeight = "0px";
+    }
+  });
+}
+
+// ACCORDION EXHIBITION
+var acc = document.getElementsByClassName("accordion--exhibition");
+
+for (var i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    var isActive = document.querySelector(".accordion--exhibition").classList.contains("active");
+    if (isActive) {
+      panel.style.maxHeight = "480px";
+    } else {
+      panel.style.maxHeight = "0px";
+    }
+  });
+}
+
+// ACCORDION SERVICES
+var acc = document.getElementsByClassName("accordion--services");
+
+for (var i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    var isActive = document.querySelector(".accordion--services").classList.contains("active");
     if (isActive) {
       panel.style.maxHeight = "230px";
     } else {
