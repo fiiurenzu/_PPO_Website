ecca359af3aef44d77c376e594a7399bbcd1385a
Flo <martiniflorent@gmail.com>
2021-06-10 11:31:07 +0200

    feat:<accordion> mise a jour taille de vw a rem

--- js/accordion.js
+++ js/accordion.js
@@ -39,7 +39,7 @@ for (var i = 0; i < acc.length; i++) {
     var panel = this.nextElementSibling;
     var isActive = document.querySelector(".accordion--services").classList.contains("active");
     if (isActive) {
-      panel.style.maxHeight = "13vw";
+      panel.style.maxHeight = "25rem";
     } else {
       panel.style.maxHeight = "0px";
     }
